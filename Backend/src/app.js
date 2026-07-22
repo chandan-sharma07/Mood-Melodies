b4375868fc33c15fb96cfa44e73b34e58762905f
Chandan_Kr_Sharma <b.techchandancs@gmail.com>
2026-07-22 12:59:13 +0530

    Allow Vercel frontend in CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://mood-melodies-one.vercel.app"
+    ],
     credentials: true,
 }));
-
 
 // Routes
 const authRoutes = require("./routes/auth.routes");
@@ -19,4 +22,4 @@ const songRoutes = require("./routes/song.routes");
 app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 
-module.exports = app;   
+module.exports = app;
