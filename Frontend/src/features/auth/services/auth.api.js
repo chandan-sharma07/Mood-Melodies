f6f057b93ef167e48ae4136b3b6308dd68df5ee8
ChandanSharma <b.techchandancs@gmail.com>
2026-08-07 04:09:47 +0530

    some changes on backend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "https://mood-melodies-backend.onrender.com";
+// const API_BASE = import.meta.env.VITE_API_URL || "https://mood-melodies-backend.onrender.com";
 
-const api = axios.create({
+/* const api = axios.create({
   baseURL: API_BASE + "/api/auth",
   withCredentials: true,
+}); */
+
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
+  withCredentials: true,
 });
+
+/* export const register = async (userData) => {
+  const { data } = await api.post("/register", userData);
+  return data;
+}; */
+
 
 export const register = async (userData) => {
   const { data } = await api.post("/register", userData);
